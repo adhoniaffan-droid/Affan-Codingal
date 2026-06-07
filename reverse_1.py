71726b0267c8d0c74b214cf8b2ddff2cd7628298
adhoniaffan-droid <adhoniaffan@gmail.com>
2026-06-07 18:55:10 +0100

    projects

---
+++ reverse_1.py
@@ -0,0 +1,13 @@
+fruits=['apple','banana','cherry']
+fruits.reverse()
+print(fruits)
+
+clothes=['shirt','jumper','trousers']
+clothes.reverse()
+print(clothes)
+
+numbers=['1','4','7','6','9']
+numbers.reverse()
+print(numbers)
+
+
---
+++ swapping.py
@@ -0,0 +1,7 @@
+x, y = 6, 8
+
+print("Before swapping:", x, y)
+
+# Swapping using tuple unpacking
+x, y= y, x
+print("Aftr swapping:", x, y)
